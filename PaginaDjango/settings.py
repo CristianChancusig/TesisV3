3118d295a53e8da9cbec5b5c2700d820b1b18dcb
CristianChancusig <kris.tian199841@gmail.com>
2022-03-12 19:45:54 -0500

    pagina

--- PaginaDjango/settings.py
+++ PaginaDjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ev^idp1v-vos7te@)fmtk_(edm-7u0!*v!y@khhy^iy=n0#gf$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','tesisv3.herokuapp.com']
 
 
 # Application definition
